526b8c05cd78a027a1e80f7e36835e6073c71ba0
sathvik-surineni <surinenisathvikteja@gmail.com>
2024-06-23 01:17:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
+// import React from 'react';
+// import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import AboutUs from './components/AboutUs/AboutUs';
+// import OurMission from './components/OurMission/OurMission';
+// import ContactUs from './components/ContactUs/ContactUs';
+// import BlogPage from './components/Blogs/BlogPage';
+// import Tools from './components/Tools/Tools'; // Import Tools component
+// import NavBar from './components/NavBar/NavBar';
+// import Footer from './components/Footer/Footer';
+
+// const AppLayout = () => {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Outlet />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// const appRouter = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <AppLayout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <AboutUs />,
+//       },
+//       {
+//         path: '/our-mission',
+//         element: <OurMission />,
+//       },
+//       {
+//         path: '/tools',
+//         element:<Tools />,
+//       },
+//       {
+//         path: '/blogs',
+//         element: <BlogPage />,
+//       },
+//       {
+//         path: '/contact',
+//         element: <ContactUs />,
+//       },
+//     ],
+//   },
+// ]);
+
+// const App = () => {
+//   return <RouterProvider router={appRouter} />;
+// };
+
+// export default App;
 import React from 'react';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createHashRouter, Outlet, RouterProvider } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import OurMission from './components/OurMission/OurMission';
 import ContactUs from './components/ContactUs/ContactUs';
 import BlogPage from './components/Blogs/BlogPage';
-import Tools from './components/Tools/Tools'; // Import Tools component
+import Tools from './components/Tools/Tools';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
@@ -20,7 +76,7 @@ const AppLayout = () => {
   );
 };
 
-const appRouter = createBrowserRouter([
+const appRouter = createHashRouter([
   {
     path: '/',
     element: <AppLayout />,
@@ -35,7 +91,7 @@ const appRouter = createBrowserRouter([
       },
       {
         path: '/tools',
-        element:<Tools />,
+        element: <Tools />,
       },
       {
         path: '/blogs',
